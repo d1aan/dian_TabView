40b4561a160a5ecdc4eb1868edc38916ee764a73
dapyaaa <alfindavi48@gmail.com>
2024-05-27 13:48:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dian_tabview/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.dian_tabview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tab = findViewById<TabLayout>(R.id.tabLayout)
+        var fragment: Fragment = RedFragment()
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+
+     tab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+         override fun onTabSelected(tab: TabLayout.Tab?) {
+             when(tab!!.position) {
+                 0 -> fragment = RedFragment()
+                 1 -> fragment = YellowFragment()
+                 2 -> fragment = BlueFragment()
+                 3 -> fragment = kulinerFragment()
+                 4 -> fragment = busanaFragment()
+             }
+
+             val fT = supportFragmentManager.beginTransaction()
+             fT.replace(R.id.frameLayout, fragment)
+             fT.commit()
+         }
+
+         override fun onTabUnselected(tab: TabLayout.Tab?) {
+         }
+
+         override fun onTabReselected(tab: TabLayout.Tab?) {
+
+         }
+     })
+    }
+}
